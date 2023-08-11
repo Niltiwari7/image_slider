6412b08e135a4d9509f5f4b5d123b05e28d5b149
Niltiwari7 <93751356+Niltiwari7@users.noreply.github.com>
2023-08-11 10:48:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.to(".imagecontainer",{
+        ease:Expo.easeInOut,
+        width:"100%",
+        stagger:5
+
+})
+
+
+gsap.to(".text h1",{
+    delay:2.5,
+    ease:Expo.easeInOut,
+    stagger:5,
+    top:"-100%",
+    
+})
+
